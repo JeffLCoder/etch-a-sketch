4d64fdf964f0e02ea3581490eb7c4191485d4ce3
JeffLCoder <jeff_lei@outlook.com>
2022-12-13 11:49:53 -0500

    create start files and write pseudo-code

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+//pseudo-code:
+// create 2 range inputs for user to choose the number of squares of each side
+//create a button for user to submit
+//clear the whole grid
+//create a container for the  grid
+//use  recursive loops to generate the grid
+// add eventListener to each grid for mouse over event
+// add hover effect
